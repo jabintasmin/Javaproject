dd51d72ee24e37d0db918890410bc2b9a79f8ec0
Jabintasmin <jabintasminqa@gmail.com>
2020-12-26 15:13:35 -0500

    Third

---
+++ Firstcode.java
@@ -0,0 +1,14 @@
+package newcode;
+
+public class Firstcode {
+    public static void main (String[] args){
+        String value = "welcome to ";
+        String value1 = "newyear 2021";
+        System.out.println(value+value1);
+        String add = "This is my first java code";
+        System.out.println(add);
+        String add1 = "i live in New york \n" ;
+        String add2 = "my address is";
+        System.out.println(add1+add2);
+    }
+}
