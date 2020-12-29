301b4dc4d1930625e6c7ac84d43786caf58695c4
Jabintasmin <jabintasminqa@gmail.com>
2020-12-29 15:13:40 -0500

    Shortdatatype

---
+++ ShortDataType.java
@@ -0,0 +1,9 @@
+package newcode;
+
+public class ShortDataType {
+    public static void main(String[] args){
+        short a = 234;
+        short b = 34;
+        System.out.print(a*b);
+    }
+}
