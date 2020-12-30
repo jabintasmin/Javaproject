ea9b2be1cb7ba95f0dfbd1e5b2df73a5ac570585
Jabintasmin <jabintasminqa@gmail.com>
2020-12-30 10:15:28 -0500

    LongDataType

---
+++ LongDataType.java
@@ -0,0 +1,10 @@
+package newcode;
+
+public class LongDataType {
+    public static void main(String[] args){
+        long num= 142345L;
+        long num1= 123459L;
+        System.out.print(num+num1);
+
+    }
+}
