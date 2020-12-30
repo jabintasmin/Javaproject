20fba684c8954f6c745b279c6a11c2ace33f4783
Jabintasmin <jabintasminqa@gmail.com>
2020-12-30 10:12:19 -0500

    Array

---
+++ ArrayDataType.java
@@ -0,0 +1,10 @@
+package newcode;
+
+public class ArrayDataType {
+    public static void main(String[] args){
+        String [] fruits = {"apple", "orange", "orange"};
+        System.out.print(fruits[2]);
+    }
+
+
+}
