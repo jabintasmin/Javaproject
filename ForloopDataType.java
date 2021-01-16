2359a1ff337146c45241a890a3041c5109cb05ee
jabintasmin <75916707+jabintasmin@users.noreply.github.com>
2021-01-16 18:20:30 -0500

    Update ForloopDataType.java

--- ForloopDataType.java
+++ ForloopDataType.java
@@ -2,7 +2,7 @@ package newcode;
 
 public class ForloopDataType {
     public static void main(String[] args){
-        for (int i = 10; i < 20; i++){
+        for (int i = 10; i < 20; i=i+2){
             System.out.println(i);
         }
     }
