c3c384e711144198c939b103e48223c33e5e6b61
Jabintasmin <jabintasminqa@gmail.com>
2021-03-20 21:01:47 -0400

    openBrowser

--- src/test/java/controller/ScriptBase.java
+++ src/test/java/controller/ScriptBase.java
@@ -2,40 +2,44 @@ package controller;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 public class ScriptBase {
-    @Test
+   //@Test
+   public WebDriver driver;
     public void init(){
+
         System.setProperty("webdriver.chrome.driver","./drivers/chromedriver");
-        WebDriver driver= new ChromeDriver();
+        driver = new ChromeDriver();
         driver.get("http://automationpractice.com/index.php");
-        driver.quit();
+        driver.findElement(By.xpath("//*[@id='header']/div[2]/div/div/nav/div[1]/a")).click();
+        //driver.quit();
 
     }
-    @Test
-    public void init2(){
-        System.setProperty("webdriver.gecko.driver","./drivers/geckodriver");
-        WebDriver driver= new FirefoxDriver();
-        driver.get("http://automationpractice.com/index.php");
-        driver.quit();
+   // @Test
+   // public void init2(){
+      //  System.setProperty("webdriver.gecko.driver","./drivers/geckodriver");
+      //  WebDriver driver= new FirefoxDriver();
+      //  driver.get("http://automationpractice.com/index.php");
+      //  driver.quit();
 
 
+    //}
+   // @Test
+   //public void webdriver(){
+      //  WebDriverManager.chromedriver().setup();
+       // WebDriver driver= new ChromeDriver();
+       // driver.get("http://automationpractice.com/index.php");
+      //  driver.quit();*\
+   // }
+   // @Test
+    //public void safariBrowser(){
+      //  WebDriver driver= new SafariDriver();
+       // driver.get("http://automationpractice.com/index.php");
+       // driver.quit();
     }
-    @Test
-    public void webdriver(){
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver= new ChromeDriver();
-        driver.get("http://automationpractice.com/index.php");
-        driver.quit();
-    }
-    @Test
-    public void safariBrowser(){
-        WebDriver driver= new SafariDriver();
-        driver.get("http://automationpractice.com/index.php");
-        driver.quit();
-    }
-}
+//}
